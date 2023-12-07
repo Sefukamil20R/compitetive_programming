c82af21fba3f145a111fcaad1976f88f203e31c0
Sefukamil20R <130300957+Sefukamil20R@users.noreply.github.com>
2023-12-07 16:12:00 +0300

    Added solution for Non-decreasing Array

---
+++ Python-Track/non-decreasing-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+            count=0
+            for i in range(len(nums)-1):
+                if nums[i] > nums[i+1]:
+                        if i >=1 and nums[i+1] < nums[i-1]:
+                            count+=1
+                            nums[i+1]=nums[i]
+                        else:
+                            nums[i]=nums[i+1]
+                            count+=1
+            return count<2
+       
+   
+       
+            
+                
+                
+                            
+                        
