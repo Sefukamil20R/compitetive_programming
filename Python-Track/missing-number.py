7290fce71d87b18321f00e61fbf9f85de31e6064
Sefukamil20R <130300957+Sefukamil20R@users.noreply.github.com>
2023-12-08 14:17:29 +0300

    Added solution for Missing Number

---
+++ Python-Track/missing-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+       
+            n = len(nums)
+            
+            for num in range(n + 1):
+                if num not in nums:
+                    return num
+
+        
