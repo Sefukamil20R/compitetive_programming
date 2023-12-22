ae2c570af0760a088acdc67a9e8176d98341adbd
Sefukamil20R <130300957+Sefukamil20R@users.noreply.github.com>
2023-12-22 20:02:17 +0300

    Added solution for Increasing Triplet Subsequence

---
+++ Python-Track/increasing-triplet-subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        x,y = nums[0],float('inf')
+        for i in range(len(nums)-1):
+            if nums[i+1] <= x:
+                x = nums[i+1]
+            elif nums[i+1] <= y:
+                y = nums[i+1]
+            elif nums[i+1] > x and nums[i+1] > y:
+                return True 
+        return False 
+
+
+
+        
