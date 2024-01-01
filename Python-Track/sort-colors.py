56782a1ffebf620e17f3ac09f095a5ee45a2a46f
Sefukamil20R <130300957+Sefukamil20R@users.noreply.github.com>
2024-01-01 12:48:34 +0300

    Added solution for Sort Colors

--- Python-Track/sort-colors.py
+++ Python-Track/sort-colors.py
@@ -4,8 +4,13 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         for i in range (1,len(nums)):
-            j=i
-            while j>0 and nums[j] < nums[j-1]:
-                nums[j],nums[j-1]=nums[j-1],nums[j]
-                j-=1
+            key=nums[i]
+            j = i-1
+            while j >= 0 and key < nums[j]:
+                nums[j+1] = nums[j]
+                j -= 1
+
+            nums[j+1]=key
+
+            
         
