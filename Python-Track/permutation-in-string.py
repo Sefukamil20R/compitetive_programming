9ff2d079ca405d0b4355f288c1fd539711409bfe
Sefukamil20R <130300957+Sefukamil20R@users.noreply.github.com>
2024-01-08 22:21:30 +0300

    Added solution for Permutation in String

--- Python-Track/permutation-in-string.py
+++ Python-Track/permutation-in-string.py
@@ -1,95 +1,17 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1count=Counter(s1)
-        k=len(s1)
-        window=Counter(s2[:k])
-        if s1count==window:
-            return True
-        for i in range(1,len(s2)-k+1):
-            window[s2[i-1]]-=1
-            if window[s2[i-1]]==0:
-                del window[s2[i-1]]
-            window[s2[i+k-1]]+=1
-            if window==s1count:
+        k = len(s1)
+        ind = k - 1
+        s1Count = Counter (s1)
+        s2Count = Counter(s2[:ind])
+        while ind < len(s2):
+            s2Count[s2[ind]] += 1
+            if s2Count == s1Count:
                 return True 
-        return False 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # S1count=Counter(s1)
-        # k=len(s1)
-        # window=Counter(s2[:k])
-        # if window==S1count:
-        #     return True 
-        # for i in range(len(s2)-k):
-        #     window[s2[i]]-=1
-        #     if window[s2[i]]==0:
-        #         del window[s2[i]]
-        #     window[s2[i+k]]+=1
-        #     if window ==S1count:
-        #         return True
-        # return False    
-
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-    #    S1count = Counter(s1)
-    #    window=  Counter(s2[:len(s1)])
-    #    if window == S1count:
-    #        return True 
-    #    k=len(s1)
-    #    for i in range(1,len(s2)-k+1):
-    #        window[s2[i-1]]-=1
-    #        if window[s2[i-1]]==0:
-    #            del window[s2[i-1]]
-    #        window[s2[i+k-1]]+=1
-    #        if window ==S1count:
-    #            return True
+            s2Count[s2[ind-k+1]] -= 1
+            if s2Count[s2[ind-k+1]] == 0:
+                del s2Count[s2[ind-k+1]]
+            ind += 1
+        return False
+              
+                  
