401d6ae959af843601b12e6233da0c1dc311ed77
Sefukamil20R <130300957+Sefukamil20R@users.noreply.github.com>
2024-02-15 23:53:36 +0300

    Added solution for Middle of the Linked List

--- Progresheet/middle-of-the-linked-list.py
+++ Progresheet/middle-of-the-linked-list.py
@@ -1,16 +1,16 @@
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
-#         self.val = val
+#         self.val = val 
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        fast = head
-        slow = head
-        
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-            
-        head = slow
-        return head
+       fast = head
+       slow = head 
+    #    as the fast moves twice than the slow when fast gets at the end or 
+    #    becomes none slow becomes at halfway so slow became athe middle node 
+       while fast and fast.next:
+           fast = fast.next.next
+           slow = slow.next
+       return slow          
+      
