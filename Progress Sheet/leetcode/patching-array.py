4cc3a9b942b02b1642df719083338b14442aa388
Sefukamil20R <130300957+Sefukamil20R@users.noreply.github.com>
2024-02-21 11:02:34 +0300

    Add solution for Patching Array

---
+++ Progress Sheet/leetcode/patching-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        cover,patches,i   = 0 , 0 , 0
+    
+       
+        while  cover < n:
+            if i < len(nums) and  nums[i] <= cover + 1 :
+                cover += nums[i]
+                i += 1
+            else:
+                patches += 1
+                cover += cover + 1
+        return patches
+        
