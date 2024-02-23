d3dfe6cfd7d5303bfa54d26b2f56293d074ad5b4
Sefukamil20R <130300957+Sefukamil20R@users.noreply.github.com>
2024-02-23 09:43:29 +0300

    Add solution for Jump Game

--- Progress Sheet/leetcode/jump-game.py
+++ Progress Sheet/leetcode/jump-game.py
@@ -1,13 +1,25 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        maxx = nums[0]
-        for i in range(1,len(nums)):
-            if maxx < i :
-                return False
-            # if maxx < nums[i] :
-            #     maxx = nums[i] + i
-            maxx = max(maxx , nums[i] + i)
+        # maxx = nums[0]
+        # for i in range(1,len(nums)):
+        #     if maxx < i :
+        #         return False
+        #     # if maxx < nums[i] :
+        #     #     maxx = nums[i] + i
+        #     maxx = max(maxx , nums[i] + i)
             
-        return True
+        # return True
+        n = len(nums)
+        goal = n-1 
+        count = 0
+        for i in range(n - 2,-1,-1):
+            if nums[i] + i >= goal:
+                goal = i
+                count += 1
+        
+        return goal  == 0
+
+
+
         
         
