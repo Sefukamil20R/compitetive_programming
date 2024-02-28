ca16147c741cabdd89f310d734c2c624b8edffc1
Sefukamil20R <130300957+Sefukamil20R@users.noreply.github.com>
2024-02-28 23:10:20 +0300

    Add solution for Find Mode in Binary Search Tree

--- Progress Sheet/leetcode/find-mode-in-binary-search-tree.py
+++ Progress Sheet/leetcode/find-mode-in-binary-search-tree.py
@@ -6,39 +6,39 @@
 #         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        visited = defaultdict(int)
-        res  = 0
-        ans = []
-        if not  root:
-            return None
-        stack = []
-        stack.append(root)
-        while stack:
-            node = stack.pop()
-            visited[node.val] += 1
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
-
-        res = max(visited.values())
-        for i in visited:
-            if visited[i] == res:
-                ans.append(i)
-        return ans
+        # visited = defaultdict(int)
+        # res  = 0
         # ans = []
-        # temp = defualtdict(int)
-        # def counters(root):
-        #     if root:
-        #         temp[root] += 1
-        #         counters(root.left)
-        #         counters(root.right)
-        # counters(root)
-        # maxx = max(temp.values())
-        # for i in counters:
-        #             if counters[i] == res:
-        #                 ans.append(i)
+        # if not  root:
+        #     return None
+        # stack = []
+        # stack.append(root)
+        # while stack:
+        #     node = stack.pop()
+        #     visited[node.val] += 1
+        #     if node.left:
+        #         stack.append(node.left)
+        #     if node.right:
+        #         stack.append(node.right)
+
+        # res = max(visited.values())
+        # for i in visited:
+        #     if visited[i] == res:
+        #         ans.append(i)
         # return ans
+        ans = []
+        temp = defaultdict(int)
+        def counters(root):
+            if root:
+                temp[root.val] += 1
+                counters(root.left)
+                counters(root.right)
+        counters(root)
+        maxx = max(temp.values())
+        for i in temp:
+                    if temp[i] == maxx:
+                        ans.append(i)
+        return ans
 
 
 
